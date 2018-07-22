100be44ad4d854d82c92934f585d6b1076d223af
Daniel Bastos <daniel@viperit.com.br>
2018-07-22 14:09:35 -0300

    Acrescentado pacote do PowerBI

--- src/components/gerencial/comercial/Comercial.vue
+++ src/components/gerencial/comercial/Comercial.vue
@@ -1,5 +1,11 @@
 <template lang="html">
-  <h1> Metas </h1>
+  <div>
+    <div class="row">
+      <div class="col-xs-12 col-md-12">
+        <iframe width="100%" height="700" src="https://app.powerbi.com/view?r=eyJrIjoiNmVkODE2N2QtMTA0Zi00ZWM1LTlmZWYtZjZhODU2MjEwMmRiIiwidCI6ImZmZGY3MTljLTcxNjQtNGYzZS04MzFiLTQ5YTBlZmQyZjQyYiJ9" frameborder="0" allowFullScreen="true"></iframe>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
--- src/router/index.js
+++ src/router/index.js
@@ -29,7 +29,7 @@ export default new Router({
     },
     {
       path: '/gerencial',
-      component: AuthLayout,
+      component: AppLayout,
       children: [
         {
           name: 'metas',
